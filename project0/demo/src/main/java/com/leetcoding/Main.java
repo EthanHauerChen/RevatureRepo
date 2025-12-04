1f730555ff4f908be94b4c418d34d399aecbb247
EthanHauerChen <ethanhchen01@gmail.com>
2025-12-04 10:18:34 -0600

    project0 first commit

---
+++ project0/demo/src/main/java/com/leetcoding/Main.java
@@ -0,0 +1,21 @@
+package com.leetcoding;
+
+/**
+ * MAIN IDEA:
+ * some sort of app that tracks and logs:
+ * completed leetcode solutions
+ * info about the leetcode problems like
+ *      description
+ *      difficulty
+ *      topics
+ * completed solutions contain
+ *      completed code
+ *      could have multiple solutions per solution "object"
+ * maybe entertain the idea of multiple users
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
